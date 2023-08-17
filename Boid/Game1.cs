3fcc2611362097a7d1dc93de7db833629a93faa0
Campbell Brown <campbellbrown093@gmail.com>
2023-08-18 11:11:28 +1200

    Small tidy up of project files

--- Boid/Game1.cs
+++ Boid/Game1.cs
@@ -6,8 +6,8 @@ namespace Boid;
 
 public class Game1 : Game
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+    GraphicsDeviceManager _graphics;
+    SpriteBatch? _spriteBatch;
 
     public Game1()
     {
@@ -18,24 +18,20 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+        {
             Exit();
-
-        // TODO: Add your update logic here
+        }
 
         base.Update(gameTime);
     }
@@ -43,9 +39,6 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
-
         base.Draw(gameTime);
     }
 }
